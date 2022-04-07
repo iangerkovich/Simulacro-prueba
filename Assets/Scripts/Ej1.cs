9fba659c957cbd6ce14c4e4b6fb044a053862ff5
iangerkovich <101577604+iangerkovich@users.noreply.github.com>
2022-04-07 08:52:31 -0300

    Termina los 2

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,29 @@
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+        Debug.Log("Los números ingresados son iguales");
+        }
+        else
+        {
+            Debug.Log("Los números ingresados NO son iguales");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,41 @@
+﻿//2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos 
+// y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+//Luego indicar cuánto dinero sobra o falta.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    public int prod1;
+    public int prod2;
+    public int prod3;
+    public int dinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sum;
+        int total;
+        int sobra;
+        sum = prod1 + prod2 + prod3;
+        total = (sum - dinero);
+        sobra = (dinero - sum);
+
+        
+       if (sum > dinero)
+        {
+            Debug.Log("Te faltan" + total + " para comprar los 3 productos");
+        }
+       else
+        {
+            Debug.Log("Te sobran " + sobra + "pesos");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
